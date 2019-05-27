6431a10ced02965f3d13801662f098bbe7309d89
CYYSILVER <immortal_youth@126.com>
2019-05-27 13:49:33 +0800

    finish v0.1

--- src/router.js
+++ src/router.js
@@ -1,24 +1,93 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventCreate from './views/EventCreate.vue'
+import EventList from './views/EventList.vue'
+import EventShow from './views/EventShow.vue'
+import NProgress from 'nprogress'
+import store from '@/store/store.js'
+import NotFound from '@/views/NotFound'
+import NetworkIssue from '@/views/NetworkIssue'
+import Example from '@/views/Example'
 
-Vue.use(Router);
+Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'hash',
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'event-list',
+      component: EventList,
+      props: true
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/event/:id',
+      name: 'event-show',
+      component: EventShow,
+      props: true,
+      beforeEnter(routeTo, routeFrom, next) {
+        store
+          .dispatch('event/fetchEvent', routeTo.params.id)
+          .then(event => {
+            routeTo.params.event = event
+            next()
+          })
+          .catch(error => {
+            if (error.response && error.response.status == 404) {
+              next({ name: '404', params: { resource: 'event' } })
+            } else {
+              next({ name: 'network-issue' })
+            }
+          })
+      }
+    },
+    {
+      path: '/eventcreate',
+      name: 'event-create',
+      component: EventCreate
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      props: true
+    },
+    {
+      path: '*',
+      redirect: {
+        name: '404',
+        params: {
+          resource: 'page'
+        }
+      }
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
+    },
+    {
+      path: '/example',
+      name: 'example',
+      component: Example
     }
   ]
-});
+})
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
